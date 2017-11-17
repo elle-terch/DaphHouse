2b69e4de268695e2dc7efec15aabfd698ecdcaa7
Lindsay <lterchin@gmail.com>
2017-11-17 09:10:38 -0800

    initial commit

---
+++ DaphHouse/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaphHouse
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Dog
+    {
+        #region Properties
+        /// <summary>
+        /// xxx
+        /// </summary>
+        public string DogName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MyProperty { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MyProperty { get; set; }
+        #endregion
+    }
+}
