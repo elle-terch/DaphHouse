cc0c9d72cd8d91367fc4625d3d1eb4902a31006c
Lindsay <lterchin@gmail.com>
2018-02-01 08:27:10 -0800

    converted project to web application

---
+++ DaphHouseUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DaphHouseUI.MvcApplication" Language="C#" %>
---
+++ DaphHouseUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DaphHouseUI.Startup))]
+namespace DaphHouseUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
